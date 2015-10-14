d66f2cf3825c3815f2ade286c95cb5af513c0cd7
Inikaam <inikaam@gmail.com>
2015-10-14 18:31:02 +0200

    PUT request

--- routes/nannies.js
+++ routes/nannies.js
@@ -27,10 +27,10 @@ router.route('/')
 					for(var i in err.errors) {
 						errors[i] = err.errors[i].message;
 					}
-					res.send(errors);
+					res.status(400).send(errors);
 				}
 				else {
-					res.json(newNanny);
+					res.status(201).json(newNanny);
 				}
 			});
 		} else {
@@ -39,31 +39,49 @@ router.route('/')
 	});
 
 router.route('/:id')
+	.all(function(req, res, next) {
+		id = req.params.id;
+		if(! /[0-9a-f]{24}/i.test(id)) {
+			res.sendStatus(400);
+		} else {
+			next();
+		}
+	})
 	.get(function(req, res) {
 		id = req.params.id;
 		
-		if(/[0-9a-f]{24}/i.test(id)) {
-			Nanny.findById(id, function (err, nanny){
-				if (err)
+		Nanny.findById(id, function (err, nanny){
+			if (err)
+				res.send('Error');
+			else if(!nanny)
+				res.status(404).send("Aucune nounou trouvée.")
+			else 
+				res.json(nanny);
+		});
+	})
+	.put(parseUrlencoded, function(req, res) {
+		id = req.params.id;
+		var isValid = true;
+		for (var index in req.body) {
+			isValid &= (req.body[index] != "" && req.body[index] != null);
+		}
+		
+		if (isValid) {
+			Nanny.findByIdAndUpdate(id, {$set: req.body}, function(err, nanny){
+				if(err)
 					res.send('Error');
-				else if(!nanny)
-					res.status(404).send("Aucune nounou trouvée.")
-				else 
-					res.json(nanny);
+				else {
+					if(! nanny)
+						res.status(404).send("Aucune nounou trouvée.");
+					else {
+						res.sendStatus(200);
+					}
+				}
 			});
-		} else {
-			res.sendStatus(400);
 		}
 	})
-
-// var nannyExample = new Nanny({
-// name: 'Myriam'
-// });
-//
-// nannyExample.save();
-//
-// Nanny.find(function(err, nannies){
-// console.info(nannies);
-// });
+	.delete(function(req, res) {
+		
+	});
 
 module.exports = router;
